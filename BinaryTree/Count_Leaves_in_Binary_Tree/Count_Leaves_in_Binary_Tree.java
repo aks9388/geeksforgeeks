26b887b6a66f6250a7c89858177bb6adc486c35d
abhishek.singh <aks9388@gmail.com>
2020-10-18 17:21:21 +0550

    Count_Leaves_in_Binary_Tree

---
+++ BinaryTree/Count_Leaves_in_Binary_Tree/Count_Leaves_in_Binary_Tree.java
@@ -0,0 +1,13 @@
+class Tree
+{
+    int countLeaves(Node root) 
+    {
+         if(root==null){
+             return 0;
+         }
+         if(root.left==null && root.right==null){
+             return 1;
+         }
+         return countLeaves(root.left) + countLeaves(root.right);
+    }
+}
